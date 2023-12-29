fa35ffb0048c2724ca5f9c9cb3f6c90c111fa9ac
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2023-12-29 10:06:23 +0500

    Triple mebel

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <div class="container mx-auto h-[90px] flex justify-between items-center">
+            <div class="w-1/3">
+                <img src="../../assets/images/mebelLogo.svg" alt="LOGO" class="w-[100px]">
+            </div>
+            <div class="flex items-center gap-6 font-[500] text-[18px]">
+                <router-link to="#">Bosh sahifa</router-link>
+                <router-link to="#">Mahsulot narxlari</router-link>
+                <router-link to="#">Portfolio</router-link>
+                <router-link to="#">Kontakt</router-link>
+                <router-link to="#">Biz haqimizda</router-link>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
